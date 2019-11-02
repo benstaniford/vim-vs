55a514e2b79dd9d0a32836aa31a5747cc3aeb88a
Ben Staniford <ben.staniford@avecto.com>
2019-11-02 23:12:25 +0000

    Adding support for VS2019

--- bin/vimvs.msbuild.bat
+++ bin/vimvs.msbuild.bat
@@ -10,12 +10,18 @@ for %%v in (2.0, 3.5, 4.0, 12.0, 14.0) do (
   )
 )
 
-:: Try to find for VS 2017
+:: Try to find for VS 2017 / 2019
 for /f "usebackq tokens=1* delims=: " %%i in (`%~dp0\vswhere -latest -requires Microsoft.Component.MSBuild`) do (
   if /i "%%i"=="installationPath" set InstallDir=%%j
 )
 
+:: On 2017 MSBuild will be here
 if exist "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" set msBuildExe=%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe
+
+:: On 2019 MSBuild will be here
+if exist "%InstallDir%\MSBuild\Current\Bin\MSBuild.exe" set msBuildExe=%InstallDir%\MSBuild\Current\Bin\MSBuild.exe
+
+echo Using MSBUILD: %msBuildExe%
 
 if not exist "%msBuildExe%" (
 	echo MSBuild not found
